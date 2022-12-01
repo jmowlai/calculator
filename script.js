46df4c95077337f52e9745d1d07fefcdeff9c2c9
Jordan <jmowlai@gmail.com>
2022-12-02 08:55:39 +1100

    Add functionality to buttons

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ const operate = function (func, a, b) {
         return divide(a, b);
     }
 };
+
+var display = document.getElementById('display');
+var firstNum = null;
+var secondNum = null;
+var operator = null;
+
+const arithButtons = document.querySelectorAll('button.arithButton');
+const oppButtons = document.querySelectorAll('button.oppButton');
+
+arithButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        appendDisplay(button.value);
+    });
+});
+
+function appendDisplay(e) {
+    display.textContent += e;
+    console.log(e)
+}
+
+oppButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        firstNum = Number(display.textContent);
+        operator = button.value;
+        console.log(firstNum);
+        console.log(button.value);
+    })
+})
