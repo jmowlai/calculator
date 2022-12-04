4a1cc1961779ad5fdbedcab2f923662f3ab01a9e
Jordan <jmowlai@gmail.com>
2022-12-04 20:21:36 +1100

    Introduce advanced functionality for arithmetic operators

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ const operate = function (func, a, b) {
 
 var display = document.getElementById('display');
 var displayFirstNum = document.getElementById('displayFirstNum');
-var firstNum = null;
-var secondNum = null;
-var operator = null;
+var firstNum = undefined;
+var secondNum = undefined;
+var operator = undefined;
 
 const arithButtons = document.querySelectorAll('button.arithButton');
 const oppButtons = document.querySelectorAll('button.oppButton');
@@ -46,30 +46,52 @@ arithButtons.forEach((button) => {
     });
 });
 
+oppButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        operateButton(button);
+    })
+});
+
+equalButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        displayFirstNum.textContent = operate(operator, Number(displayFirstNum.textContent), Number(display.textContent));
+        display.textContent = undefined;
+        operator = undefined;
+    })
+})
+
+clearButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        clear();
+    })
+})
+
 function appendDisplay(e) {
     display.textContent += e;
 }
 
-oppButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (!displayFirstNum.textContent) { 
-            operator = button.value;
-            displayFirstNum.textContent = Number(display.textContent);
-            display.textContent = undefined;
-        }
-        else {
-            firstNum = Number(operate(operator, Number(displayFirstNum.textContent), Number(display.textContent)));
-            operator = button.value;
-            display.textContent = undefined;
-            displayFirstNum.textContent = firstNum;
-        }
-    })
-});
-
-clearButton.forEach((button) => {
-    button.addEventListener('click', () => {
+function operateButton(button) {
+    if (!displayFirstNum.textContent) { 
+        operator = button.value;
+        displayFirstNum.textContent = Number(display.textContent);
         display.textContent = undefined;
-        displayFirstNum.textContent = undefined;
-        operator = undefined;
-    })
-})
+    }
+    else if (!operator) {
+        firstNum = Number(operate(button.value, Number(displayFirstNum.textContent), Number(display.textContent)));
+        operator = button.value;
+        display.textContent = undefined;
+        displayFirstNum.textContent = firstNum;
+    }
+    else {
+        firstNum = Number(operate(operator, Number(displayFirstNum.textContent), Number(display.textContent)));
+        operator = button.value;
+        display.textContent = undefined;
+        displayFirstNum.textContent = firstNum;
+    }
+}
+
+function clear() {
+    display.textContent = undefined;
+    displayFirstNum.textContent = undefined;
+    operator = undefined;
+} 
